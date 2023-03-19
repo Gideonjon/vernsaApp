6b68efd1e2beed5089c67e314b9147bf6bcfff99
Gideonjon <gideonjones63@gmail.com>
2023-03-19 22:27:55 +0100

    whakheee

--- app/src/main/java/com/example/vernsa/PersonalAccount.kt
+++ app/src/main/java/com/example/vernsa/PersonalAccount.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.vernsa.databinding.FragmentPersonalAccountBinding
 
 
 class PersonalAccount : Fragment() {
-  private var _bi
+  private var _binding : FragmentPersonalAccountBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_account, container, false)
+
+     _binding = FragmentPersonalAccountBinding.inflate(inflater,container,false)
+     val view = binding.root
+
+
+
+
+     return view
     }
 
 
